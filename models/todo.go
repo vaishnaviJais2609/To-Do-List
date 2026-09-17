9d254556aebcf6c489c85b140e9a3575131fbc2d
Vaishnavi Jaiswal <vaishnavijais2609@gmail.com>
2026-09-17 16:04:47 +0530

    fix: json struct tags

--- models/todo.go
+++ models/todo.go
@@ -1,7 +1,7 @@
 package models
 
 type Todo struct {
-	ID        int    `json: "id"`
-	Title     string `json: "title"`
-	Completed bool   `json: "completed"`
+	ID        int    `json:"id"`
+	Title     string `json:"title"`
+	Completed bool   `json:"completed"`
 }
